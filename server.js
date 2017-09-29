8b7f1c55e899579ddff68800affc3d887a53d8eb
zyk0 <77777mill@gmail.com>
2017-09-29 23:20:51 +0500

    delete module color

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
-var color = require('colors');
+//var color = require('colors');
 var request = require('request');
 var favicon = require('serve-favicon');
 var fs = require("fs");
@@ -15,29 +15,30 @@ app.use(favicon(__dirname + '/public/images/a.ico')); // favicon
 //routing
 app.get("/", function(req,res){
 	res.render('home', {title: "Homepage"});
-	//logger.info('main page'. grey);
+	
 });
 app.get("/about", function(req,res){
 	res.render('about', {title: "About"});
-	//logger.info('about'. grey);
 });
 app.get("/contact", function(req,res){
 	res.render('contact', {title: "Contact"});
-	//logger.info('Entering'. grey);
 });
 app.get("/news", function(req,res){
 	res.render('news', {title: "News"});
-	//logger.info('news about solar systems'. grey );
 });
 
-// 404error
 app.use(function(req, res, next){
 	res.status(404);
 	res.render('404');
 	//console.log("page not found! error 404". yellow);
 });
 
-//port
+/*
 app.listen(2017, function(){
 	console.log("Sun Energy". cyan, "127.0.0.1 port 2017". magenta);
+});
+*/
+app.set('port', (process.env.PORT || 2017));
+app.listen(app.get('port'), function() {
+  console.log("Sun Energy", app.get('port'));
 });
